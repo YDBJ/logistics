04c9d58ce4a6808cc2691c74778f2ccc70e3b133
found <found@qq.com>
2022-07-04 00:04:16 +0800

    feat:完成首页

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,21 @@
+// index.ts
+// 获取应用实例
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+  },
+  // 事件处理函数
+  bindViewTap() {
+
+  },
+  onLoad() {
+
+  },
+  getUserProfile() {
+
+  },
+  getUserInfo() {
+
+  }
+})
